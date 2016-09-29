c43044b5c1cd6e25ea77ea4e14340f33cbf17e9a
zhuanxuhit <893051481@qq.com>
2016-09-29 10:26:18 +0800

    lab1 partIII 测试通过,但是使用串行方式，待优化为并行

--- mit/src/mapreduce/schedule.go
+++ mit/src/mapreduce/schedule.go
@@ -22,7 +22,28 @@ func (mr *Master) schedule(phase jobPhase) {
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
 	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+	// 总共有ntasks个任务需要给worker，此处使用串行的方式，肯定会非常慢，测试花了3.02s
+	for i := 0; i < ntasks; i++ {
+		worker := <- mr.registerChannel
+
+		debug("Master: schdule worker %s\n", worker)
+		arg := &DoTaskArgs{
+			JobName:mr.jobName,
+			File:mr.files[i],
+			Phase:phase,
+			TaskNumber:i,
+			NumOtherPhase:nios,
+		}
+		ok := call(worker, "Worker.DoTask", arg, new(struct{}))
+		if ok == false {
+			fmt.Printf("Worker: RPC %s DoTask error\n", worker)
+		} else {
+			// 此处如果直接写，会导致阻塞，因为只有此处for进行读取
+			go func() {
+				mr.registerChannel <- worker
+			}()
+		}
+	}
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
