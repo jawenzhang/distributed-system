302987b59f82051e3fbf20093c755b4051bdfa93
zhuanxuhit <893051481@qq.com>
2016-09-29 10:35:48 +0800

    lab1 partIII 改为并行

--- mit/src/mapreduce/schedule.go
+++ mit/src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -23,27 +26,32 @@ func (mr *Master) schedule(phase jobPhase) {
 	// multiple tasks.
 	//
 	// 总共有ntasks个任务需要给worker，此处使用串行的方式，肯定会非常慢，测试花了3.02s
+	// 并行后花费时间2.352s
+	var wg sync.WaitGroup
 	for i := 0; i < ntasks; i++ {
-		worker := <- mr.registerChannel
+		go func(i int) {
+			wg.Add(1)
+			worker := <- mr.registerChannel
 
-		debug("Master: schdule worker %s\n", worker)
-		arg := &DoTaskArgs{
-			JobName:mr.jobName,
-			File:mr.files[i],
-			Phase:phase,
-			TaskNumber:i,
-			NumOtherPhase:nios,
-		}
-		ok := call(worker, "Worker.DoTask", arg, new(struct{}))
-		if ok == false {
-			fmt.Printf("Worker: RPC %s DoTask error\n", worker)
-		} else {
-			// 此处如果直接写，会导致阻塞，因为只有此处for进行读取
-			go func() {
-				mr.registerChannel <- worker
-			}()
-		}
+			debug("Master: schdule worker %s\n", worker)
+			arg := &DoTaskArgs{
+				JobName:mr.jobName,
+				File:mr.files[i],
+				Phase:phase,
+				TaskNumber:i,
+				NumOtherPhase:nios,
+			}
+			ok := call(worker, "Worker.DoTask", arg, new(struct{}))
+			if ok == false {
+				fmt.Printf("Worker: RPC %s DoTask error\n", worker)
+			} else {
+				go func() {
+					mr.registerChannel <- worker
+				}()
+			}
+			wg.Done()
+		}(i)
 	}
-
+	wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
